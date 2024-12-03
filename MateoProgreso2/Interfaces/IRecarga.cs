7efed3ab39cc97401786a81a2c279a23aca7c7f1
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-03 09:51:18 -0500

    Solucion en IRecarga

--- MateoProgreso2/Interfaces/IRecarga.cs
+++ MateoProgreso2/Interfaces/IRecarga.cs
@@ -1,16 +1,14 @@
 ﻿using MateoProgreso2.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MateoProgreso2.Interfaces
 {
-    public class IRecarga
+    // Definición de la interfaz IRecarga
+    public interface IRecarga
     {
+        // Métodos sin cuerpo (sólo las firmas de los métodos)
         List<Recarga> DevuelveListRecarga();
         Recarga DevuelveRecarg(int Id);
-        Boolean CrearRecarga(Recarga recarga);
+        bool CrearRecarga(Recarga recarga);
     }
 }
--- MateoProgreso2/Repositories/RecargaRepository.cs
+++ MateoProgreso2/Repositories/RecargaRepository.cs
@@ -2,17 +2,20 @@
 using MateoProgreso2.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+
 
 namespace MateoProgreso2.Repositories
 {
+   
     public class RecargaRepository : IRecarga
     {
+        
         public string _fileName = Path.Combine(FileSystem.AppDataDirectory, "MateoSotomayor.txt");
 
+       
         public bool CrearRecarga(Recarga recarga)
         {
             try
@@ -20,44 +23,49 @@ namespace MateoProgreso2.Repositories
                 var recargas = DevuelveListRecarga();
 
                 recargas.Add(recarga);
+
                 var json = JsonSerializer.Serialize(recargas);
+
                 File.WriteAllText(_fileName, json);
 
-                return true;
+                return true; 
             }
             catch
             {
-                return false;
+                return false; 
             }
         }
 
+       
         public List<Recarga> DevuelveListRecarga()
         {
             try
             {
-
+                
                 if (File.Exists(_fileName))
                 {
+                    
                     var json = File.ReadAllText(_fileName);
+
+                   
                     return JsonSerializer.Deserialize<List<Recarga>>(json) ?? new List<Recarga>();
                 }
+
+               
                 return new List<Recarga>();
             }
             catch
             {
-                return new List<Recarga>();
+                return new List<Recarga>();  
             }
         }
 
+       
         public Recarga DevuelveRecarg(int Id)
         {
-            var recargas = DevuelveListRecarga();
-            return recargas.ElementAtOrDefault(Id);
+            var recargas = DevuelveListRecarga();  
+
+            return recargas.FirstOrDefault(r => r.Id == Id);
         }
     }
 }
-
-
-
-
-
