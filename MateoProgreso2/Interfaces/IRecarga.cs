c2c8dea45a7b1fdcd153fcb31ddef240d940f2ff
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-03 11:37:38 -0500

    Mejorar el From

--- MateoProgreso2/Interfaces/IRecarga.cs
+++ MateoProgreso2/Interfaces/IRecarga.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 namespace MateoProgreso2.Interfaces
 {
-    // Definición de la interfaz IRecarga
+   
     public interface IRecarga
     {
-        // Métodos sin cuerpo (sólo las firmas de los métodos)
+
         List<Recarga> DevuelveListRecarga();
         Recarga DevuelveRecarg(int Id);
         bool CrearRecarga(Recarga recarga);
