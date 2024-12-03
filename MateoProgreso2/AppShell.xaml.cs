125fb271335e1278d8b784d8ef10f5a75412420a
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-03 08:22:06 -0500

    Agregar archivos de proyecto.

---
+++ MateoProgreso2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MateoProgreso2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MateoProgreso2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MateoProgreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
