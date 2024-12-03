311b3b69d2783f2b00c57cddd595450d41fbaeba
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2024-12-03 08:45:29 -0500

    Cree la clase repositorio

---
+++ MateoProgreso2/Interfaces/IRecarga.cs
@@ -0,0 +1,16 @@
+﻿using MateoProgreso2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MateoProgreso2.Interfaces
+{
+    internal class IRecarga
+    {
+        List<Recarga> DevuelveListRecarga();
+        Recarga DevuelveRecarg(int Id);
+        Boolean CrearRecarga(Recarga recarga);
+    }
+}
--- MateoProgreso2/MRecarga.xaml.cs
+++ MateoProgreso2/MRecarga.xaml.cs
@@ -1,9 +1,63 @@
+using MateoProgreso2.Interfaces;
+using MateoProgreso2.Models;
+using MateoProgreso2.Repositories;
+
+
 namespace MateoProgreso2;
 
 public partial class MRecarga : ContentPage
 {
-	public MRecarga()
-	{
-		InitializeComponent();
-	}
+    public Recarga recarga;
+    IRecarga _recargaRepository;
+    public List<Recarga> recargaList;
+    public string _fileName = Path.Combine(FileSystem.AppDataDirectory, "CrhystelVelasco.txt");
+    public MRecarga()
+    {
+        InitializeComponent();
+        recarga = new Recarga();
+        BindingContext = recarga;
+        _recargaRepository = new RecargaRepository();
+
+    }
+
+
+    private async void Buton1_Clicked(object sender, EventArgs e)
+    {
+        var nuevaRecarga = new Recarga
+        {
+            Nombre = mateoSotomayor_entry1.Text,
+            NumeroTelefono = mateoSotomayor_entry.Text,
+            MensajeResultado = "Recarga exitosa"
+        };
+
+        if (_recargaRepository.CrearRecarga(nuevaRecarga))
+        {
+            mateoSotomayor_label2.Text = "Recarga guardada con éxito.";
+            CargarRecargas();
+            await DisplayAlert("Alert", "Recarga guardada con éxito.\"", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Error al guardar la recarga.\"", "OK");
+            mateoSotomayor_label2.Text = "Error al guardar la recarga.";
+        }
+
+
+
+    }
+    private void CargarRecargas()
+    {
+
+        var recargas = _recargaRepository.DevuelveListRecarga();
+
+
+        var recargasTexto = string.Join("\n", recargas.Select(r => $"{r.Nombre} - {r.NumeroTelefono}"));
+
+        mateoSotomayor_label2.Text = recargasTexto;
+    }
+    private void Buton2_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Grid());
+
+    }
 }
